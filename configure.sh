5cbfb0b028302957590687f7d5bba51e94980817
sirh3e <sirh3e@protonmail.com>
2021-02-11 19:14:20 +0100

    added comments and setup for rust nigthly for the project

--- configure.sh
+++ configure.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#db init
 echo "'run first 'create database voting;'"
 
+#scripts docker infrastructor
 chomod +x scripts/docker_compose.sh
+
+#rust infrastructor
+rustup override set nightly
