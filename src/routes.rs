ec69abc527dfbf4b5a0fdce7cce0aaaeb217f0eb
SirCremefresh <donato.wolfisberg@gmail.com>
2021-02-14 01:11:08 +0100

    refactored some code

--- src/routes.rs
+++ src/routes.rs
@@ -2,7 +2,7 @@ use super::models::*;
 use super::pool::DbConn;
 use super::schema::votings::dsl::*;
 
-use crate::utils::{hash_string, generate_uuid};
+use crate::utils::{generate_uuid, hash_string};
 use diesel::insert_into;
 use diesel::prelude::*;
 use rocket::response::status::BadRequest;
@@ -44,26 +44,16 @@ pub fn create_voting(
     conn: DbConn,
     input: Json<CreateVotingRequest>,
 ) -> Result<Json<CreateVotingResponse>, BadRequest<String>> {
-    match input.name.len() {
-        5...60 => {}
-        _ => {
-            return Err(BadRequest(Some(String::from(
-                "Name length must be between 5 and 60 characters",
-            ))));
-        }
-    };
+    validate_create_voting_request(&input)?;
 
     let admin_key = generate_uuid();
     let generated_admin_key_hash = hash_string(&admin_key);
 
-    let generated_voting_id = insert_into(votings)
-        .values((
-            name.eq(&input.name),
-            admin_key_hash.eq(generated_admin_key_hash),
-        ))
-        .returning(voting_id)
-        .get_result(&*conn)
-        .unwrap();
+    let generated_voting_id = insert_voting(
+        conn,
+        &input.name,
+        generated_admin_key_hash,
+    );
 
     let create_voting_response = CreateVotingResponse {
         voting_id: generated_voting_id,
@@ -71,4 +61,31 @@ pub fn create_voting(
     };
 
     Ok(Json(create_voting_response))
+}
+
+fn validate_create_voting_request(input: &Json<CreateVotingRequest>) -> Result<(), BadRequest<String>> {
+    match input.name.len() {
+        5..=60 => Ok(()),
+        _ => Err(BadRequest(Some(String::from(
+            "Name length must be between 5 and 60 characters",
+        ))))
+    }?;
+    match input.polls.len() {
+        1..=100 => Ok(()),
+        _ => Err(BadRequest(Some(String::from(
+            "It must have between 1 and 100 polls",
+        ))))
+    }
+}
+
+fn insert_voting(conn: DbConn, voting_name: &String, voting_admin_key_hash: String) -> String {
+    let generated_voting_id = insert_into(votings)
+        .values((
+            name.eq(&voting_name),
+            admin_key_hash.eq(voting_admin_key_hash),
+        ))
+        .returning(voting_id)
+        .get_result(&*conn)
+        .unwrap();
+    generated_voting_id
 }
