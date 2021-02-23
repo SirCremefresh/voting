e8962030c5a99754b2b7987a3b636682009556a7
Donato Wolfisberg <donato@wolfisberg.dev>
2021-02-23 19:29:55 +0000

    validate poll

--- src/routes.rs
+++ src/routes.rs
@@ -164,7 +164,29 @@ fn validate_create_voting_request(input: &Json<CreateVotingRequest>) -> Result<(
             reason: "Voting must have between 1 and 100 polls".to_string(),
             status: Status::BadRequest,
         }),
+    }?;
+    validate_create_voting_polls_request(&input.polls)
+}
+
+fn validate_create_voting_polls_request(polls: &Vec<CreateVotingPollRequest>) -> Result<(), ErrorResponse> {
+    for poll in polls {
+        match poll.name.len() {
+            5..=60 => Ok(()),
+            _ => Err(ErrorResponse {
+                reason: "Poll Name length must be between 5 and 60 characters".to_string(),
+                status: Status::BadRequest,
+            }),
+        }?;
+        match poll.description.len() {
+            5..=60 => Ok(()),
+            _ => Err(ErrorResponse {
+                reason: "Poll Description length must be between 5 and 60 characters".to_string(),
+                status: Status::BadRequest,
+            }),
+        }?;
     }
+
+    Ok(())
 }
 
 fn insert_voting(
