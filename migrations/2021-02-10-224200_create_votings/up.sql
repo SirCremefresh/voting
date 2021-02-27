82fc24eb6d7ed25584d2fda9af6e55b814072a1c
SirCremefresh <donato.wolfisberg@gmail.com>
2021-02-27 23:38:48 +0100

    fix poll_results sql

--- migrations/2021-02-10-224200_create_votings/up.sql
+++ migrations/2021-02-10-224200_create_votings/up.sql
@@ -5,63 +5,63 @@ CREATE
 
 CREATE TABLE votings
 (
-    id      VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
-    admin_key_hash VARCHAR(64) NOT NULL,
-    name           VARCHAR(64) NOT NULL,
-    active_poll_index INT DEFAULT NULL
+    id                VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
+    admin_key_hash    VARCHAR(64) NOT NULL,
+    name              VARCHAR(64) NOT NULL,
+    active_poll_index INT                     DEFAULT NULL
 );
 
 CREATE TABLE polls
 (
-    id      VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
-    sequenz_number INT NOT NULL,
-    voting_fk   VARCHAR(36) NOT NULL
+    id             VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
+    sequenz_number INT         NOT NULL,
+    voting_fk      VARCHAR(36) NOT NULL
         CONSTRAINT polls_votings_id_fk
-            REFERENCES votings(id)
+            REFERENCES votings (id)
             ON DELETE CASCADE,
-    name        VARCHAR(64) NOT NULL,
-    description VARCHAR(64) NOT NULL
+    name           VARCHAR(64) NOT NULL,
+    description    VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE voters 
+CREATE TABLE voters
 (
-    id      VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id             VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
     voter_key_hash VARCHAR(64) NOT NULL,
-    voting_fk   VARCHAR(36) NOT NULL
+    voting_fk      VARCHAR(36) NOT NULL
         CONSTRAINT voters_votings_id_fk
-            REFERENCES votings(id)
+            REFERENCES votings (id)
             ON DELETE CASCADE,
-    username VARCHAR(64) NOT NULL
+    username       VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE votes (
-    id      VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
-    poll_fk   VARCHAR(36) NOT NULL
-        CONSTRAINT votes_polls_id_fk
-            REFERENCES polls(id)
-            ON DELETE CASCADE,
-    voter_fk   VARCHAR(36) NOT NULL
-        CONSTRAINT votes_voters_id_fk
-            REFERENCES voters(id)
-            ON DELETE CASCADE,
-    answer BOOLEAN NULL
-);
-
-CREATE VIEW poll_results as
+CREATE TABLE votes
 (
-select p.id,
+    id       VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
+    poll_fk  VARCHAR(36) NOT NULL
+        CONSTRAINT votes_polls_id_fk
+            REFERENCES polls (id)
+            ON DELETE CASCADE,
+    voter_fk VARCHAR(36) NOT NULL
+        CONSTRAINT votes_voters_id_fk
+            REFERENCES voters (id)
+            ON DELETE CASCADE,
+    answer   BOOLEAN     NULL
+);
+
+CREATE VIEW poll_results AS
+(
+SELECT p.id,
        p.sequenz_number,
        p.voting_fk,
        p.name,
        p.description,
-       count(votes_accept.id)                                                     as votes_accept,
-       count(votes_decline.id)                                                    as votes_decline,
-       count(votes_abstain.id)                                                    as votes_abstain,
-       count(votes_accept.id) + count(votes_decline.id) + count(votes_abstain.id) as votes_total
-from polls p
-         left join votes votes_accept on p.id = votes_accept.poll_fk and votes_accept.answer = True
-         left join votes votes_decline on p.id = votes_decline.poll_fk and votes_decline.answer = False
-         left join votes votes_abstain on p.id = votes_abstain.poll_fk and votes_abstain.answer IS NULL
-group by p.id, p.name, p.description, p.sequenz_number, p.voting_fk
-order by p.sequenz_number
+       COUNT(CASE WHEN v.answer THEN 1 END)         AS votes_accept,
+       COUNT(CASE WHEN v.answer = FALSE THEN 1 END) AS votes_decline,
+       COUNT(v.id) - COUNT(CASE WHEN v.answer = TRUE THEN 1 END) - COUNT(CASE WHEN v.answer = FALSE THEN 1 END)
+                                                    AS votes_abstain,
+       COUNT(v.id)                                  AS votes_total
+FROM polls p
+         LEFT JOIN votes v ON p.id = v.poll_fk
+GROUP BY p.id, p.name, p.description, p.sequenz_number, p.voting_fk
+ORDER BY p.sequenz_number
     );
